a2d9dcedbb510f6f4cb6e0f103ffb88e7308c934
PMedvedeva777 <Megaskill535@gmail.com>
2026-09-10 15:14:27 +0500

    Task2

---
+++ Tyuiu.MedvedevaPI.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using Tyuiu.MedvedevaPI.Sprint0.Task2.V0;
+namespace Tyuiu.MedvedevaPI.Sprint0.Task2.V0.Test;
+using Tyuiu.MedvedevaPI.Sprint0.Task2.V0.Lib;
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Полина"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
