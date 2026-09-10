25eb93bd48341ef2093c95ea15caecb45b934eb0
PMedvedeva777 <Megaskill535@gmail.com>
2026-09-10 13:35:14 +0500

    task1

---
+++ Tyuiu.MedvedevaPI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MedvedevaPI.Sprint0.Task1.V0;
+namespace Tyuiu.MedvedevaPI.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,World");
+            string name = "Полина";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
