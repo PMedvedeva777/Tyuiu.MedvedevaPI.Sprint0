9422b1b07036c9fe29bac7fadb2313b2ecaf9cd1
PMedvedeva777 <Megaskill535@gmail.com>
2026-09-10 16:35:40 +0500

    task3

---
+++ Tyuiu.MedvedevaPI.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyuiu.MedvedevaPI.Sprint0.Task3.V0.Test;
+using Tyuiu.MedvedevaPI.Sprint0.Task3.V0.Lib;
+
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+
---
+++ Tyuiu.MedvedevaPI.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.MedvedevaPI.Sprint0.Task3.V0;
+using Tyuiu.MedvedevaPI.Sprint0.Task3.V0.Lib;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
