81e6e35ed03eb8088105c2e7958c46fff34d433e
PMedvedeva777 <Megaskill535@gmail.com>
2026-09-12 13:22:41 +0500

    Task4

---
+++ Tyuiu.MedvedevaPI.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.MedvedevaPI.Sprint0.Task4.V0;
+using Tyuiu.MedvedevaPI.Sprint0.Task4.V0.Lib;
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            Console.WriteLine(DataService.Division(5, 5));
+            Console.ReadKey();
+        }
+    }
